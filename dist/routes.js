096f9b41751ac3b16f9054cf6167988ad702ced8
prog6bteam2cecyte cecyte <equipo2prog6b@gmail.com>
2025-06-01 14:21:57 +0000

    post y get

--- dist/routes.js
+++ dist/routes.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Routes = void 0;
+const campoDisciplinar_routes_1 = require("./modules/campoDisciplinar/routes/campoDisciplinar.routes");
 const usuario_routes_1 = require("./modules/usuario/routes/usuario.routes");
 class Routes {
     constructor() {
         this.usuarioRoutes = new usuario_routes_1.UsuarioRoutes();
+        this.campoDisciplinarRoutes = new campoDisciplinar_routes_1.CampoDisciplinarRoutes();
     }
     routes(app) {
         this.usuarioRoutes.routes(app);
+        this.campoDisciplinarRoutes.routes(app);
     }
 }
 exports.Routes = Routes;
